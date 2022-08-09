d0c8582302b5c7aa15715964752643f97a733ceb
Antonin Folliasson <antonin@aitio.co>
2022-08-09 07:53:34 +0200

    useref effective

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState, useRef } from "react"
+
+function App() {
+  const [name, setName] = useState('')
+  const renderCount = useRef(1)
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1
+  })
+
+  return (
+    <div className="App">
+      <h1>My Full Page</h1>
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <div>My name is {name}</div>
+      <div>I rendered {renderCount.current} times</div>
+    </div>
+  )
+
+}
+
+export default App
